fa563040024404132b2ce82fa096ada9c40ed46e
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-29 23:17:12 +0200

    add createGame function

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function main() {}
+function createGame() {
+  const game = document.getElementById("game-window");
+  for (let i = 0; i < 18; i++) {
+    const row = game.insertRow(i);
+    row.id = "row_" + i;
+    for (let j = 0; j < 18; j++) {
+      const cell = row.insertCell(j);
+      cell.id = "cell_" + i + "_" + j;
+    }
+  }
+}
+
+function main() {
+  createGame();
+}
 
 document.addEventListener("DOMContentLoaded", main);
