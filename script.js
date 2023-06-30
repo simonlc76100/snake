9ac238150308f326100203db030e1afe56cada4a
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-30 17:53:34 +0200

    add createSnake function

--- script.js
+++ script.js
@@ -11,23 +11,40 @@ function createGameWindow(rows, cellsPerRow) {
 }
 
 function getGameMatrix(cells, rows, cellsPerRow) {
-  let gameMatrix = new Array(rows).fill().map(() => new Array(cellsPerRow));
+  let gameMatrix = new Array(rows)
+    .fill()
+    .map(() => new Array(cellsPerRow).fill(0));
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cellsPerRow; j++) {
-      gameMatrix[i][j] = cells[i * cellsPerRow + j];
+      if (cells[i * cellsPerRow + j].textContent === "") gameMatrix[i][j] = 0;
+      else {
+        gameMatrix[i][j] = cells[i * cellsPerRow + j].textContent;
+      }
     }
   }
   return gameMatrix;
 }
 
+function createSnake(gameMatrix, snake) {
+  gameMatrix[snake.i][snake.j] = 1;
+  console.log("initial snake pos", gameMatrix);
+}
+
 function main() {
   let rows = 50;
   let cellsPerRow = 80;
+
+  let snake = {
+    i: 9,
+    j: 9,
+  };
+
   createGameWindow(rows, cellsPerRow);
   const cells = document.querySelectorAll('td[id^="cell_"]');
   let gameMatrix = getGameMatrix(cells, rows, cellsPerRow);
-  console.log(gameMatrix);
+
+  createSnake(gameMatrix, snake);
 }
 
 document.addEventListener("DOMContentLoaded", main);
