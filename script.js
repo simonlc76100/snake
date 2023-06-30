01ada109a12d9ea3b6fade6719c9f850c1c716d9
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-06-30 12:45:46 +0200

    add getGameMatrix function

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function createGameWindow() {
+function createGameWindow(rows, cellsPerRow) {
   const game = document.getElementById("game-window");
-  for (let i = 0; i < 27; i++) {
+  for (let i = 0; i < rows; i++) {
     const row = game.insertRow(i);
     row.id = "row_" + i;
-    for (let j = 0; j < 45; j++) {
+    for (let j = 0; j < cellsPerRow; j++) {
       const cell = row.insertCell(j);
       cell.id = "cell_" + i + "_" + j;
     }
   }
 }
 
+function getGameMatrix(cells, rows, cellsPerRow) {
+  let gameMatrix = new Array(rows).fill().map(() => new Array(cellsPerRow));
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cellsPerRow; j++) {
+      gameMatrix[i][j] = cells[i * cellsPerRow + j];
+    }
+  }
+  return gameMatrix;
+}
+
 function main() {
-  createGameWindow();
+  let rows = 50;
+  let cellsPerRow = 80;
+  createGameWindow(rows, cellsPerRow);
+  const cells = document.querySelectorAll('td[id^="cell_"]');
+  let gameMatrix = getGameMatrix(cells, rows, cellsPerRow);
+  console.log(gameMatrix);
 }
 
 document.addEventListener("DOMContentLoaded", main);
